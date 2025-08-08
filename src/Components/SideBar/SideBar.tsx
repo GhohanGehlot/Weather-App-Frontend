e330eba47e334c02b00d664dde42dea723c35893
Your Name <you@example.com>
2025-08-08 15:09:38 +0530

    added basic layout with sidebar and main data

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,8 +1,8 @@
 function SideBar(){
     return(
-        <>
+        <div className="basis-3/12 flex flex-col  items-center text-white h-full rounded-tl-3xl rounded-bl-3xl" style={{background: 'rgba(255 , 255 ,255 , 0.81)'}}>
          SideBar
-        </>
+        </div>
     )
 }
 
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,16 @@
 import MainData from "../Components/Main Data/MainData";
 import SideBar from "../Components/SideBar/SideBar";
+import NightImage from "../assets/night.jpg"
 
 function Home(){
     return(
-        <div>
-            <SideBar />
-            <MainData />
+        <div className="h-[100vh]" style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)) , url(${NightImage})`}}>
+            <div className="h-full mx-8 py-8 flex flex-row justify-center items-center">
+                <SideBar />
+                <MainData />
+            </div>
         </div>
+        
     )
 }
 
