55dbcb8bae3087ab01de045e9783c17cabb72c14
Your Name <you@example.com>
2025-08-08 14:34:15 +0530

    Tailwind config

---
+++ src/Components/Main Data/MainData.tsx
@@ -0,0 +1,9 @@
+function MainData(){
+    return(
+        <>
+        Main Data
+        </>
+    )
+}
+
+export default MainData;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,14 @@
+import MainData from "../Components/Main Data/MainData";
+import SideBar from "../Components/SideBar/SideBar";
+
+function Home(){
+    return(
+        <div>
+            <SideBar />
+            <MainData />
+        </div>
+    )
+}
+
+
+export default Home;
